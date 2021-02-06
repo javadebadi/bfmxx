06fa400b97210f9e3d029ff352bd12ecd4fbba8e
javadebadi <javad.ebadi.1990@gmail.com>
2021-02-06 23:12:05 +0330

    Created a view and html template for a url to create account

--- bookkeeping/urls.py
+++ bookkeeping/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
+from .views import create_account
+
 urlpatterns = [
-    
+    path('account/create/', create_account),
 ]
--- bookkeeping/views.py
+++ bookkeeping/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create_account(request):
+    """The functions creates an account by handling the request
+    """
+    context={"Hello":"Hellow"}
+    return render(request, 'bookkeeping/account/create.html',context=context)
