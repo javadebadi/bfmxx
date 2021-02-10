c1d4614bf692da010a123690d9b5a76743e97751
javadebadi <javad.ebadi.1990@gmail.com>
2021-02-10 22:24:18 +0330

    Updated models in order to fill database from a csv file from hard disk or memory

--- bookkeeping/models.py
+++ bookkeeping/models.py
@@ -1,3 +1,4 @@
+import io
 import csv
 from django.db import models
 
@@ -76,16 +77,37 @@ class Account(models.Model):
     def __str__(self):
         return f'{self.account_name} --- {self.account_type}'
     
-    def fill_from_csv(self, file_path="bookkeeping/accounts.csv"):
-        account = Account()
-        with open(file_path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                obj, created = Account.objects.get_or_create(
-                    account_name = row['account_name'],
-                    account_type = row['account_type'],
-                    account_category = row['account_category'],
-                    account_description = row['account_description'],
-                )
-                if created is False:
-                    obj.save()
+    def _create_object_from_dictionary(self, row, update=True):
+        try:
+            obj, created = Account.objects.get_or_create(
+                            account_name = row['account_name'],
+                            account_type = row['account_type'],
+                            account_category = row['account_category'],
+                            account_description = row['account_description'],
+                        )
+            if created is False:
+                obj.save()
+        except:
+            obj = Account.objects.get(account_name=row['account_name'])
+            obj.account_type = row['account_type']
+            obj.account_category = row['account_category']
+            obj.account_description = row['account_description']
+            if update is True:
+                obj.save()
+        return obj
+
+    def fill_from_csv(self, from_memory=True, file_in_memory=None, file_path="bookkeeping/accounts.csv"):
+        if from_memory is True:
+            if file_in_memory is None:
+                raise ValueError("When csv file is read from memeory the file_in_memory arg must be given")        
+            for chunk in file_in_memory.chunks():
+                csv_file_string = chunk.decode("utf-8")
+                csv_file_in_memory = io.StringIO(csv_file_string)
+                reader = csv.DictReader(csv_file_in_memory, delimiter=',', quotechar='|')
+                for row in reader:
+                    self._create_object_from_dictionary(row)
+        else:
+            with open(file_path, newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                        self._create_object_from_dictionary(row)
