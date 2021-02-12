c489ffa1e5e1d7773b8a1470f69b54ee12429b46
javadebadi <javad.ebadi.1990@gmail.com>
2021-02-12 17:18:43 +0330

    Added new form

--- bookkeeping/forms.py
+++ bookkeeping/forms.py
@@ -1,4 +1,16 @@
 from django import forms
+from django.forms import ModelForm
+from .models import Account
 
 class AccountUploadFileForm(forms.Form):
     file = forms.FileField()
+
+class AccountCreateForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ["account_name", "account_type", "account_category", "account_description"]
+        widgets = {
+            "account_description": forms.Textarea,
+            "account_name": forms.TextInput,
+            "account_type": forms.RadioSelect,
+        }
